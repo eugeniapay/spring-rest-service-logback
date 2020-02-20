4b9a9815fb46fb95dcd9d5c6262a848f969dcd05
eugeniapay <33683624+eugeniapay@users.noreply.github.com>
2020-02-20 20:57:51 +0800

    Update GreetingController.java

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -12,9 +12,12 @@ public class GreetingController {
 	private static final String template = "Good Evening, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	private static final org.slf4j.Logger LOGGER = org.slf4j.LoggerFactory.getLogger(GreetingController.class);	
+
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		System.out.println("--- <Application Log> name: " + name + " ---");
+		//System.out.println("--- <Application Log> name: " + name + " ---");
+		LOGGER.info("[Application Log /greeting] name: ", name);
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
