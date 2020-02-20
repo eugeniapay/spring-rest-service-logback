49a4466e0e6aab63f5aa61985e6ea405e7d06c2e
eugeniapay <33683624+eugeniapay@users.noreply.github.com>
2020-02-20 22:16:43 +0800

    Update GreetingController.java

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -16,8 +16,10 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		System.out.println("--- <Application Log> name: " + name + " ---");
+		int cnt = counter.incrementAndGet();
+		
+		System.out.println("--- <Application Log> id : " + cnt + ", name: " + name + " ---");
 		//LOGGER.debug("[Application Log /greeting] name: ", name);
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return new Greeting(cnt, String.format(template, name));
 	}
 }
